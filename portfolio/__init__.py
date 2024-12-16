01a80f384112e41c9f5a43dd99cccc9b677c74bf
AndyB-WHG <andrew.bond2@gmail.com>
2024-12-16 21:24:22 +0000

    Added 'Home page' images and styling.

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -2,9 +2,42 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects = [
+    {
+        "name": "Habit tracking app with Python and MongoDB",
+        "thumb": "img/habit-tracking.png",
+        "hero": "img/habit-tracking-hero.png",
+        "categories": ["python", "web"],
+        "slug": "habit-tracking",
+        "prod": "https://habit-tracker-igk0.onrender.com",
+    },
+    {
+        "name": "Microblog app with Python and MongoDB",
+        "thumb": "img/online-blog-freepic-dot-com.jpg",
+        "hero": "img/online-blog-freepic-dot-com.jpg",
+        "categories": ["python", "web"],
+        "slug": "microblog",
+        "prod": "https://python-web-microblog-1-3pe5.onrender.com"
+    },
+     {
+        "name": "Personal finance tracking app with React",
+        "thumb": "img/personal-finance.png",
+        "hero": "img/personal-finance.png",
+        "categories": ["react", "javascript"],
+        "slug": "personal-finance",
+    },
+    {
+        "name": "REST API Documentation with Postman and Swagger",
+        "thumb": "img/rest-api-docs.png",
+        "hero": "img/rest-api-docs.png",
+        "categories": ["writing"],
+        "slug": "api-docs",
+    },
+]
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", projects=projects)
 
 
 @app.route("/about")
